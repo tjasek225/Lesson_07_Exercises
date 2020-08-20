25e09b3872a01d6adf8b739f0a4cbfc2949f349b
Tibor Jašek <tibor.jasek@riganti.cz>
2020-08-20 16:36:59 +0200

    Created new Console Application project. Added .gitignore file.

---
+++ Lesson_07_Exercises.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_07_Exercises.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
